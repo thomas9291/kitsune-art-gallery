2ece65f4e24538f6ffcd48e51c68bf2d5d18b5c6
thomas <130037065+thomas9291@users.noreply.github.com>
2023-11-29 16:11:51 +0100

    deleting run

--- src/pages/artDetail/[id].js
+++ src/pages/artDetail/[id].js
@@ -3,17 +3,60 @@ import { useRouter } from "next/router";
 import useSWR from "swr";
 import DetailCart from "../../components/DetailCart";
 import LoadingComponent from "../../components/loadingComponent";
+import Link from "next/link";
+import { s3Client } from "../../../utils";
+import { DeleteObjectCommand } from "@aws-sdk/client-s3";
 
 export default function DetailPage() {
   const router = useRouter();
   const { id } = router.query;
   const { data: artDetail, isLoading } = useSWR(`/api/artData/${id}`);
+
+  async function handlerDeleteArt(id) {
+    const command = new DeleteObjectCommand({
+      Bucket: process.env.NEXT_PUBLIC_AWS_BUCKET_NAME,
+      Key: artDetail.name,
+    });
+    if (confirm("are you sure you want to delete?")) {
+      try {
+        await s3Client.send(command);
+        console.log("s3 object success deleted!");
+      } catch (error) {
+        console.log(error);
+      }
+      await fetch(`/api/artData/${id}`, {
+        method: "DELETE",
+      });
+      router.push("/");
+    }
+  }
   if (isLoading) {
     return <LoadingComponent />;
   }
+  const btnStyle = {
+    backgroundColor: "red",
+    color: "white",
+    padding: "6px",
+    borderRadius: "5px",
+    marginTop: "20px",
+  };
   return (
     <div className="flex flex-col items-center m-2">
-      <DetailCart nameArt={artDetail.name} src={artDetail.url} />
+      <Link href={"/"} className="text-sky-400/50">
+        Back to the Home Page
+      </Link>
+      <div>
+        <button onClick={() => handlerDeleteArt(artDetail.id)} style={btnStyle}>
+          Delete Art
+        </button>
+      </div>
+      <DetailCart
+        nameArt={artDetail.name
+          .replaceAll("_", " ")
+          .replace(".jpg", "")
+          .toUpperCase()}
+        src={artDetail.url}
+      />
     </div>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import useSWR from "swr";
 import React, { useState } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 import { UploadImage } from "../components/UploadImageForm";
 import LoadingComponent from "../components/loadingComponent";
@@ -15,28 +16,29 @@ import "../styles/swiper.module.css";
 import { SwiperContainer } from "../components/swiperContainer";
 
 export default function HomePage() {
+  const router = useRouter();
+  const [art, setArt] = useState("");
+  const [loading, setLoading] = useState(false);
   const { data: artData, isLoading } = useSWR("/api/artData", {
     fallbackData: [],
   });
 
-  const [art, setArt] = useState("");
   async function handlerArt(data) {
+    setLoading(true);
     setArt(data);
     const urlImage = `https://kitsune-gallery1234.s3.eu-central-1.amazonaws.com/${data.name}`;
-    const artName = data.name
-      .replace(".jpg", "")
-      .replaceAll("_", " ")
-      .toUpperCase();
 
     const response = await fetch("/api/artData", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ url: urlImage, name: artName }),
+      body: JSON.stringify({ url: urlImage, name: data.name }),
     });
     if (response.ok) {
       console.log("success data fetched!");
+      router.reload();
+      setLoading(false);
     }
   }
 
@@ -48,6 +50,7 @@ export default function HomePage() {
   if (isLoading) {
     return <LoadingComponent />;
   }
+
   return (
     <div className="flex flex-col items-center justify-center relative m-5">
       <div className="m-4">
@@ -55,12 +58,16 @@ export default function HomePage() {
       </div>
       <div className=" m-5 flex">
         <SwiperContainer>
+          {loading && <LoadingComponent />}
           {artData.map((item) => {
             return (
               <SwiperSlide style={swiperStyle} key={item.id}>
                 <DetailCart
                   src={item.url}
-                  nameArt={item.name}
+                  nameArt={item.name
+                    .replaceAll("_", " ")
+                    .replace(".jpg", "")
+                    .toUpperCase()}
                   linkedId={
                     <Link href={`/artDetail/${item.id}`}>Show Detail</Link>
                   }
