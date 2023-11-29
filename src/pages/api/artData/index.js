127b257777de011e31519fb66b658606ec00daf4
thomas <130037065+thomas9291@users.noreply.github.com>
2023-11-29 11:23:58 +0100

    data fetching run

--- src/pages/api/artData/index.js
+++ src/pages/api/artData/index.js
@@ -1,14 +1,28 @@
 import { ddbClient } from "../../../../utils";
-import { PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { PutItemCommand, ScanCommand } from "@aws-sdk/client-dynamodb";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
 import { v4 as randomId } from "uuid";
 
 export default async function handler(req, res) {
   const artId = randomId();
+  if (req.method === "GET") {
+    const params = {
+      TableName: process.env.NEXT_PUBLIC_TABLE_NAME,
+    };
+    try {
+      const data = await ddbClient.send(new ScanCommand(params));
+      const unmashalledItems = data.Items?.map((item) => unmarshall(item));
+      res.status(200).json(unmashalledItems);
+      return unmashalledItems;
+    } catch (error) {
+      console.log(error);
+    }
+  }
   if (req.method === "POST") {
     const body = req.body;
     console.log("body from api: ", body);
     const params = {
-      TableName: "kitsune8731",
+      TableName: process.env.NEXT_PUBLIC_TABLE_NAME,
       Item: {
         id: { S: artId },
         name: { S: body.name },
